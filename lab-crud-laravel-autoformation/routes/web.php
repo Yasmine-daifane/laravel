ee83ecbd9cc3a60a2c753369fa7120ab4ea69761
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2023-10-27 13:39:45 +0100

    Update web.php

--- lab-crud-laravel-autoformation/routes/web.php
+++ lab-crud-laravel-autoformation/routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+        //  chapitre L'orem   
+    //  communiquer avec une base  de donnee avec laravel  
+    // l'orem c'st le demunitif de Object Relationnel Mapping  
+    // ce  sont des classes qui vont  permettre d'interagir avec les donnee en BD qui vont permettre  de  les representent  sous forme d'objet 
+// a la place de creer  la table et creer les infos d'interieur  dans la ravel on a : 
+// - on va utiliser le systeme  de migration 
+// creer une migration qui va permettre de rajouter les infos  dans notre BD (nomme notre migration)
+// php artisan make:migration create_admins_table
