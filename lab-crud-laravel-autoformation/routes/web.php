e0698c6b83e5b04c36839bc73988aca26999ddd3
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2023-10-27 16:41:04 +0100

    push

--- lab-crud-laravel-autoformation/routes/web.php
+++ lab-crud-laravel-autoformation/routes/web.php
@@ -20,7 +20,13 @@ on va affexter les propieter de cet objet (admin)
 sauvgarder les donner using methode save dans notre BD 
 notre BD sqlite view : https://sqliteviewer.app/?ref=vscode#/database.sqlite/table/admins/
 le slug est utile just pour la creation de un seul admin une seul fois il est unique 
-utilise Post pour recuperer des infos par exemple recuperer tous les admin depuis ma DB 
+utilise Post pour recuperer des infos par exemple recuperer tous les admin depuis ma DB using le smethode statique 
+mthode die and debug pour regarder a quoi ressemble un objet par exemple le type d'un retour , debouguer une variable 
+le return de cette methode est collection alors collection c'est quelque chose qui se comporte comme un tableau ms contient des methodes specifique 
+first () methode sur  les collection de laravel qui fais le retourne de 1er element 
+ using find () methode recupurer un admin specifique en donne l'id de ce admin 
+ findorfail methode  sa va etre pratique si vous ne  voulez pas executer le reste de code s'il n'a pas trouve dans l'enregistrement 
+ querybuilder va permettre de concevoir des requetes des condition des join 
 */
 
 // {"title":"yasmine","slug":"mon int","content":"le admin de cette interface est yasminele admin de cette interface est yasmine",
@@ -33,14 +39,22 @@ Route::get('/', function () {
 
             Route::prefix('/blog')->name('blog.')->group(function()  {
                 Route::get('/' ,function (Request $request) {
+                    // sauvgarde des donner 
+                    // $admin = new \App\Models\Admin();
+                    // $admin->title = 'yasmine';
+                    //   $admin ->slug = 'me';
+                    //    $admin-> content = 'le admin de cette interface est yasminele admin de cette interface est yasmine';
+                    //     $admin->save();
 
-                    $admin = new \App\Models\Admin();
-                    $admin->title = 'yasmine';
-                      $admin ->slug = 'me';
-                       $admin-> content = 'le admin de cette interface est yasminele admin de cette interface est yasmine';
-                        $admin->save();
-
-                         return $admin ;
+                    //      return $admin ;
+                      // rucuperer les donner 
+                    //    return \App\Models\Admin::all(['title','id', 'content']) ;
+                            //    $posts = \App\Models\Admin::all(['title','id', 'content']) ;
+                                 $posts = \App\Models\Admin::paginate(2 ,['id', 'title']) ;
+                            //  dd($posts[0] ->title ) ;
+                            // dd($posts -> first());
+                            // dd($posts) ;
+                              return ($posts) ;
                     return [
                         "link" => \route('blog.show',['slug' => 'article' ,'id'=> 16]),
     
